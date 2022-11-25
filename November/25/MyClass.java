55fe33bfef71faa09f067bf5b87eeab2fafcb0c2
Shabari_K_S <95925800+Shabari-K-S@users.noreply.github.com>
2022-11-25 16:56:35 +0000

    Add files via upload

---
+++ November/25/MyClass.java
@@ -0,0 +1,7 @@
+public class MyClass{
+	public static void main(String args[]){
+		int x=10,y=25;
+		int z=~x+y+1;
+		System.out.println("The Subraction of two number using - operator = "+ z);
+	}
+}
