8f9e986e3294323915b8cb48dc92819484e27299
Shabari_K_S <95925800+Shabari-K-S@users.noreply.github.com>
2022-11-26 15:14:03 +0000

    Update ChartoASCII.java

--- November/24/ChartoASCII.java
+++ November/24/ChartoASCII.java
@@ -1,7 +1,7 @@
 public class ChartoASCII {
     public static void main(String[] args) {
         char a = 'S';
-        int s = a;
+        int s = a;      // we are changing the char to ascii in this line
         System.out.println("The ASCII value of '"+a+"' this character is '"+s+"'");
     }
 }
