41b66cce923ca1d9bf0968d066ab61335d585f68
Shabari_K_S <95925800+Shabari-K-S@users.noreply.github.com>
2022-11-26 15:06:37 +0000

    Update WithoutSubractionOperator.java

--- November/25/WithoutSubractionOperator.java
+++ November/25/WithoutSubractionOperator.java
@@ -1,7 +1,9 @@
+// In this code we are subractiong two number withou using minus(-) operator
+
 public class WithoutSubractionOperator{
 	public static void main(String args[]){
-		int x=10,y=25;
-		int z=~x+y+1;
+		int x=10,y=25;		//    Logic is 
+		int z=~x+y+1;		// z = ~(10)+25+1  => -11+25+1 => -11+26 => 15
 		System.out.println("The Subraction of two number using - operator = "+ z);
 	}
 }
