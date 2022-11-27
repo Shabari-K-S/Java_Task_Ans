4de0e47d4824db0ae2c9f8775d537776215e1174
Shabari_K_S <95925800+Shabari-K-S@users.noreply.github.com>
2022-11-27 12:00:38 +0000

    Add files via upload

---
+++ November/26/PrivateMainFun.java
@@ -0,0 +1,14 @@
+public class PrivateMainFun{
+    private static void main(String[] args) {
+
+        /*
+            Can we declare a main method as private in Java?
+
+                    Yes, we can declare the main method as private in Java.
+
+                    It compiles successfully without any errors but at the runtime, it says that the main method is not public.
+        */
+
+        System.out.println("This program show some runtime error...");
+    }
+}
